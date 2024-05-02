8c3b4f1e5e5da941d78443907d71d6d876277e52
SantyJustyna <justyna_santy@hotmail.com>
2024-05-02 12:20:57 +0200

    14.2

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.patterns.prototype.library;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class LibraryTestSuite {
+    @Test
+    void testGetBooks() {
+
+        //Given
+        Library library = new Library("Library 1");
+        Book book1 = new Book("Title1", "Author1", LocalDate.of(2020, 1, 2));
+        library.getBooks().add(book1);
+        library.getBooks().add(new Book("Title2", "Author2", LocalDate.of(2021, 4, 2)));
+        library.getBooks().add(new Book("Title3", "Author3", LocalDate.of(2004, 10, 20)));
+
+        //making a shallow clone of library
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = library.shallowCopy();
+            clonedLibrary.setName("Library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //making a deep copy of library
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Library 3");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //When
+        library.getBooks().remove(book1);
+
+        //Then
+        System.out.println(library);
+        System.out.println(clonedLibrary);
+        System.out.println(deepClonedLibrary);
+        assertEquals(2, library.getBooks().size());
+        assertEquals(2, clonedLibrary.getBooks().size());
+        assertEquals(3, deepClonedLibrary.getBooks().size());
+        assertEquals(clonedLibrary.getBooks(), library.getBooks());
+        assertNotEquals(deepClonedLibrary.getBooks(), library.getBooks());
+
+    }
+}
